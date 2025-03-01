e3ef7b17c45e4b5752b6f9186b3e12ff1f0b71cb
jvictory225@gmail.com <jvictory225@gmail.com>
2025-03-01 16:49:22 +0100

    kj noknkon

--- src/api/authAxios.js
+++ src/api/authAxios.js
@@ -5,8 +5,8 @@ import { AuthContext } from '../context/AuthContext';
 
 // Create an instance of axios with default settings
 const authAxios = axios.create({
-  // baseURL: 'http://127.0.0.1:8000/',
-  baseURL: 'https://vtu-h5yu.onrender.com/',
+  baseURL: 'http://127.0.0.1:8000/',
+  // baseURL: 'https://vtu-h5yu.onrender.com/',
   headers: {
     'Content-Type': 'application/json',
   },
--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://vtu-h5yu.onrender.com/',
-  // baseURL: 'http://127.0.0.1:8000/',
+  // baseURL: 'https://vtu-h5yu.onrender.com/',
+  baseURL: 'http://127.0.0.1:8000/',
   headers: {
     'Content-Type': 'application/json',
   },
