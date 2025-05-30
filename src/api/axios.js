2634d7175229e0a1f17ff9efff64b9734979f65b
Joseph Victory <131410352+Josephvic278@users.noreply.github.com>
2025-05-30 12:47:58 +0100

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://vtu-h5yu.onrender.com/',
+  baseURL: 'https://vtu-l5ar.onrender.com/',
   // baseURL: 'http://127.0.0.1:8000/',
   headers: {
     'Content-Type': 'application/json',
